ce6d0d1f8439d0426c338e080daebfa483d1fe82
evgenydrainov <79319450+evgenydrainov@users.noreply.github.com>
2024-10-26 09:33:51 +0300

    commit

---
+++ PrepodRate/Controllers/ProfessorController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrepodRate.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace PrepodRate.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProfessorController : ControllerBase
+{
+	private readonly ApplicationDbContext _db;
+
+	public ProfessorController(ApplicationDbContext db)
+	{
+		_db = db;
+	}
+
+	[HttpGet("all")]
+	public async Task<IEnumerable<Professor>> GetAllProfessorsAsync()
+	{
+		return await _db.Professors.ToListAsync();
+	}
+
+	[HttpGet("{id:int?}")]
+	public async Task<Professor> Get(int? id = null)
+	{
+		var prof = await _db.Professors.FirstOrDefaultAsync(x => x.Id == (id ?? 1));
+		return prof;
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> AddProfessor([FromBody] Professor prof)
+	{
+		await _db.Professors.AddAsync(prof);
+		await _db.SaveChangesAsync();
+		return NoContent();
+	}
+
+	[HttpGet("test")]
+	public string GetTest()
+	{
+		return "Test";
+	}
+}
+
+public class Professor
+{
+	[JsonPropertyName("Id")]
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; }
+
+	[JsonPropertyName("Name")]
+	public string Name { get; set; }
+
+	[JsonPropertyName("Surname")]
+	public string Surname { get; set; }
+
+	[JsonPropertyName("Patronymic")]
+	public string Patronymic { get; set; }
+
+	[JsonPropertyName("University")]
+	public string University { get; set; }
+}
---
+++ PrepodRate/Controllers/ReviewController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PrepodRate.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PrepodRate.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReviewController : ControllerBase
+{
+	private readonly ApplicationDbContext _db;
+
+	public ReviewController(ApplicationDbContext db)
+	{
+		_db = db;
+	}
+
+	[HttpGet("all")]
+	public async Task<IEnumerable<Review>> GetAllReviewsAsync()
+	{
+		return await _db.Reviews.ToListAsync();
+	}
+
+	[HttpGet("{id:int?}")]
+	public async Task<Review> Get(int? id = null)
+	{
+		return await _db.Reviews.FirstOrDefaultAsync(x => x.Id == (id ?? 1));
+	}
+
+	[HttpGet("professor/{professorId:int}")]
+	public async Task<IEnumerable<Review>> GetByProfessorId(int professorId)
+	{
+		List<Review> result = new List<Review>();
+		await _db.Reviews.ForEachAsync(review =>
+		{
+			if (review.ProfessorId == professorId) {
+				result.Add(review);
+			}
+		});
+		return result;
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> AddReview([FromBody] Review review)
+	{
+		await _db.Reviews.AddAsync(review);
+		await _db.SaveChangesAsync();
+		return NoContent();
+	}
+}
+
+public class Review
+{
+	[JsonPropertyName("Id")]
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int Id { get; set; }
+
+	[JsonPropertyName("ProfessorId")]
+	public int ProfessorId { get; set; }
+
+	[JsonPropertyName("Rating")]
+	public float Rating { get; set; }
+
+	[JsonPropertyName("Body")]
+	public string Body { get; set; }
+}
